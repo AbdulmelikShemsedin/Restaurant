015f20cf9e463eb2f69f10418d0c3f9f329c2a95
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2024-04-12 15:29:22 +0300

    still front page

--- Restaurant.js
+++ Restaurant.js
@@ -2,6 +2,9 @@ const start = () => {
     $(".page1").ready(function() {
         $(".page1 .name").addClass("animate__animated animate__bounce");
         $(".page1 img").addClass("animate__animated animate__slideInRight");
+        $(".page1 img").hover(function(){
+            $(".page1 img").css('width','300px');
+        })
     })
 }
 
